fa2bc5273aad5b126ee9605662e2e006a6d8e2b7
Rakha Wisesa Rozaqtama <61936919+Rakha-Rozaqtama@users.noreply.github.com>
2021-11-26 14:13:29 +0700

    fixing blade templating

--- resources/views/layouts/landing_page/header.blade.php
+++ resources/views/layouts/landing_page/header.blade.php
@@ -1,6 +1,6 @@
 <header id="responsive-header">
     <nav class="navbar navbar-light bg-cl-primary navbar-expand-md" id="responsive-nav">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/index">
             <img class="img-fluid ml-5 logo-size mt-1 mb-1" id="logo" src="{{ asset('assets/logo-brand.svg') }}" alt="logo-brand">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active align-self-center mt-3">
-                <a class="nav-link cl-primary font-type-secondary font-size-quaternary cl-white-mobile active-now" href="/">Home<span class="sr-only">(current)</span></a>
+                <a class="nav-link cl-primary font-type-secondary font-size-quaternary cl-white-mobile active-now" href="/index">Home<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item active align-self-center mt-3">
               <a class="nav-link cl-primary font-type-secondary font-size-quaternary cl-white-mobile" href="/about">About Us</a>
