617120abd9fa6d39c2ebd432e06b87182cdadccd
Rakha Wisesa Rozaqtama <61936919+Rakha-Rozaqtama@users.noreply.github.com>
2021-11-30 14:07:41 +0700

    modifying style footer

--- resources/views/layouts/landing_page/footer.blade.php
+++ resources/views/layouts/landing_page/footer.blade.php
@@ -1,33 +1,33 @@
 <!--Desktop Footer-->
 <div id="footer-desktop">
-  <div class="d-flex flex-row justify-content-around pt-5 bg-cl-secondary p-5">
+  <div class="d-flex flex-row justify-content-center pt-5 bg-cl-secondary p-5">
     <div class="d-flex flex-column justify-content-center">
       <img src="{{ asset('images/logo/sfd-wh.svg') }}" alt="Logo" class="image-fluid img-small-footer">
-      <h4 class="font-type-tertiary font-size-tertiary mt-3 cl-quaternary">License</h4>
-      <p class="width-container-small cl-secondary font-type-tertiary font-size-septenary">Smartfunding Pte. Ltd.
+      <h4 class="font-type-tertiary font-footer-primary mt-3 cl-quaternary">License</h4>
+      <p class="width-container-small cl-secondary font-footer-secondary font-size-septenary">Smartfunding Pte. Ltd.
         Regulated by Monetary Authority of Singapore (MAS). License no: CMS-100637-1 Capital Market Services (Dealing in
         Securities)</p>
     </div>
     <div class="d-flex flex-column justify-content-around">
       <div class="d-flex flex-column justify-content-start ml-3">
-        <h4 class="font-type-tertiary font-size-quaternary cl-quaternary">Get in Touch</h4>
-        <p class="width-container-small cl-secondary font-type-tertiary font-size-septenary">Endokemi kaning. Bepir
+        <h4 class="font-type-tertiary font-footer-primary cl-quaternary">Get in Touch</h4>
+        <p class="width-container-small cl-secondary font-type-tertiary font-footer-secondary">Endokemi kaning. Bepir
           detregt. Vabel sovöpör. Milingar. Bigt seryrat. </p>
       </div>
       <div class="d-flex flex-column justify-content-start ml-3">
-        <h4 class="font-type-tertiary font-size-quaternary cl-quaternary">Address</h4>
-        <p class="width-container-small cl-secondary font-type-tertiary font-size-septenary">71, Ayer Rajah Crescent,
+        <h4 class="font-type-tertiary font-footer-primary cl-quaternary">Address</h4>
+        <p class="width-container-small cl-secondary font-type-tertiary font-footer-secondary">71, Ayer Rajah Crescent,
           <br />#06-06, Singapore <br />139951.</p>
       </div>
     </div>
     <div class="d-flex flex-column justify-content-around">
       <div class="d-flex flex-column justify-content-start ml-3">
-        <h4 class="font-type-tertiary font-size-quaternary cl-quaternary">Hotline</h4>
-        <p class="width-container-small cl-secondary font-type-tertiary font-size-septenary">+60 9111 3111</p>
+        <h4 class="font-type-tertiary font-footer-primary cl-quaternary">Hotline</h4>
+        <p class="width-container-small cl-secondary font-type-tertiary font-footer-secondary">+60 9111 3111</p>
       </div>
       <div class="d-flex flex-column justify-content-start ml-3">
-        <h4 class="font-type-tertiary font-size-quaternary cl-quaternary">Email</h4>
-        <p class="width-container-small cl-secondary font-type-tertiary font-size-septenary">hi@smartfunding.sg</p>
+        <h4 class="font-type-tertiary font-footer-primary cl-quaternary">Email</h4>
+        <p class="width-container-small cl-secondary font-type-tertiary font-footer-secondary">hi@smartfunding.sg</p>
       </div>
       <div class="d-flex flex-row justify-content-around">
         <img src="assets/Facebook.svg" alt="Facebook" class="image-fluid img-icon-mini">
@@ -39,36 +39,37 @@
   </div>
   <div class="d-flex flex-row justify-content-around bg-cl-footer p-2">
     <div class="d-flex flex-row justify-content-start">
-      <h5 class="font-type-tertiary font-size-septenary cl-secondary">© 2020 All rights reserved.</h5>
-      <h5 class="font-type-tertiary font-size-septenary cl-quaternary">Smartfunding Pte. Ltd</h5>
+      <h5 class="font-type-tertiary font-footer-secondary cl-secondary">© 2020 All rights reserved.</h5>
+      <h5 class="font-type-tertiary font-footer-secondary cl-quaternary">Smartfunding Pte. Ltd</h5>
     </div>
-    <h5 class="font-type-tertiary font-size-septenary cl-octonary">Privacy policy</h5>
-    <h5 class="font-type-tertiary font-size-septenary cl-octonary">Terms of use</h5>
+    <h5 class="font-type-tertiary font-footer-secondary cl-octonary">Privacy policy</h5>
+    <h5 class="font-type-tertiary font-footer-secondary cl-octonary">Terms of use</h5>
   </div>
 </div>
+
 <!--Mobile Footer-->
 <div id="footer-mobile">
   <div class="d-flex flex-column justify-content-around bg-cl-secondary">
     <div class="d-flex flex-column justify-content-around bg-cl-secondary p-5">
       <img src="{{ asset('images/logo/sfd-wh.svg') }}" alt="Logo" class="image-fluid img-small-footer align-self-center">
-      <h4 class="font-type-tertiary font-size-tertiary mt-3 cl-quaternary align-self-center">License</h4>
+      <h4 class="font-type-tertiary font-footer-primary mt-3 cl-quaternary align-self-center">License</h4>
       <p
-        class="width-container-small cl-secondary font-type-tertiary font-size-quaternary align-self-center text-center">
+        class="width-container-small cl-secondary font-type-tertiary font-footer-secondary align-self-center text-center">
         Smartfunding Pte. Ltd. Regulated by Monetary Authority of Singapore (MAS). License no: CMS-100637-1 Capital
         Market Services (Dealing in Securities)</p>
-      <h4 class="font-type-tertiary font-size-tertiary cl-quaternary align-self-center mt-3">Get in Touch</h4>
-      <p class="width-container-small cl-secondary font-type-tertiary font-size-quaternary align-self-center text-center">Endokemi
+      <h4 class="font-type-tertiary font-footer-primary cl-quaternary align-self-center mt-3">Get in Touch</h4>
+      <p class="width-container-small cl-secondary font-type-tertiary font-footer-secondary align-self-center text-center">Endokemi
         kaning. Bepir detregt. Vabel sovöpör. Milingar. Bigt seryrat. </p>
-      <h4 class="font-type-tertiary font-size-tertiary cl-quaternary align-self-center mt-3">Address</h4>
-      <p class="cl-secondary font-type-tertiary font-size-quaternary align-self-center text-center">71, Ayer Rajah
+      <h4 class="font-type-tertiary font-footer-primary cl-quaternary align-self-center mt-3">Address</h4>
+      <p class="cl-secondary font-type-tertiary font-footer-secondary align-self-center text-center">71, Ayer Rajah
         Crescent, <br class="text-center" />#06-06, Singapore <br class="text-center" />139951.</p>
-      <h4 class="font-type-tertiary font-size-tertiary cl-quaternary align-self-center mt-3">Hotline</h4>
+      <h4 class="font-type-tertiary font-footer-primary cl-quaternary align-self-center mt-3">Hotline</h4>
       <p
-        class="width-container-small cl-secondary font-type-tertiary font-size-quaternary align-self-center text-center">
+        class="width-container-small cl-secondary font-type-tertiary font-footer-secondary align-self-center text-center">
         +60 9111 3111</p>
-      <h4 class="font-type-tertiary font-size-tertiary cl-quaternary mt-3 align-self-center">Email</h4>
+      <h4 class="font-type-tertiary font-footer-primary cl-quaternary mt-3 align-self-center">Email</h4>
       <p
-        class="width-container-small cl-secondary font-type-tertiary font-size-quaternary align-self-center text-center">
+        class="width-container-small cl-secondary font-type-tertiary font-footer-secondary align-self-center text-center">
         hi@smartfunding.sg</p>
       <div class="d-flex flex-row justify-content-around mt-4">
         <img src="{{ asset('assets/Facebook.svg') }}" alt="Facebook" class="image-fluid img-icon-mini">
@@ -79,11 +80,11 @@
     </div>
     <div class="d-flex flex-column justify-content-around mt-5 bg-cl-footer p-3">
       <div class="d-flex flex-row justify-content-around align-self-center mb-3">
-        <h5 class="font-type-tertiary font-size-septenary cl-octonary align-self-center">Privacy policy</h5>
-        <h5 class="ml-5 font-type-tertiary font-size-septenary cl-octonary align-self-center">Terms of use</h5>
+        <h5 class="font-type-tertiary font-footer-secondary cl-octonary align-self-center">Privacy policy</h5>
+        <h5 class="ml-5 font-type-tertiary font-footer-secondary cl-octonary align-self-center">Terms of use</h5>
       </div>
-      <h5 class="font-type-tertiary font-size-septenary cl-secondary align-self-center">© 2020 All rights reserved.</h5>
-      <h5 class="font-type-tertiary font-size-septenary cl-quaternary align-self-center">Smartfunding Pte. Ltd</h5>
+      <h5 class="font-type-tertiary font-footer-secondary cl-secondary align-self-center">© 2020 All rights reserved.</h5>
+      <h5 class="font-type-tertiary font-footer-secondary cl-quaternary align-self-center">Smartfunding Pte. Ltd</h5>
     </div>
   </div>
 </div>
