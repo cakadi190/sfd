14346477134e0a74b62c57f92a3bda04b017c9d8
Rakha Wisesa Rozaqtama <61936919+Rakha-Rozaqtama@users.noreply.github.com>
2021-12-24 11:03:18 +0700

    add new algorithm for tiles chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,6 +51,7 @@ class HomeController extends Controller
       $fourthMonth = $this->countProfit(PaymentSequence::whereBetween("paid_at", [$lastLimit2, $lastLimit1])->get());
       $fifthMonth = $this->countProfit(PaymentSequence::whereBetween("paid_at", [$lastLimit1, $lastPayment->paid_at])->get());
       $data_tiles = [$firstMonth, $secondMonth, $thirdMonth, $fourthMonth, $fifthMonth];
+      
 
       # Data Pie Chart
       # Today
