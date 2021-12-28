1fcc474e3532dce316ea19e79559e54c1489b353
Rakha Wisesa Rozaqtama <61936919+Rakha-Rozaqtama@users.noreply.github.com>
2021-12-28 19:07:11 +0700

    add newest UI revision

--- resources/views/layouts/landing_page/header.blade.php
+++ resources/views/layouts/landing_page/header.blade.php
@@ -1,7 +1,8 @@
 <header id="responsive-header-desktop">
   <nav class="navbar navbar-light bg-cl-primary navbar-expand-md" id="responsive-nav">
     <a class="navbar-brand" href="/">
-      <img class="img-fluid ml-3 logo-size mt-1 mb-1" id="logo" src="{{ asset('images/logo/sfd-bl.svg') }}" height="50px" alt="logo-brand" />
+      {{--  <img class="img-fluid ml-4 logo-size mt-1 mb-1" id="logo" src="{{ asset('images/logo/sfd-bl.svg') }}" height="50px" alt="logo-brand" />  --}}
+      <img class="img-fluid logo-size mt-1 mb-1" id="logo" src="{{ asset('images/logo/sfd-bl.svg') }}" height="50px" alt="logo-brand" />
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,9 +21,6 @@
         <li class="nav-item align-self-center">
           <a href="/register" class="nav-link pt-2 pb-2 pr-4 pl-4 btn-nav bg-cl-secondary cl-secondary font-type-secondary font-size-septenary align-items-center" id="btn-responsive-mobile">Apply Loan</a>
         </li>
-        {{--  <li class="nav-item align-self-center">
-          <a href="//app.sf-direct.tri-niche.com/" class="nav-link pt-2 pb-2 btn-paynow cl-paynow font-type-primary font-size-septenary align-self-center text-center" id="btn-responsive-mobile">Pay Now</a>
-        </li>  --}}
         <li class="nav-item align-self-center">
           <a href="//app.sf-direct.tri-niche.com/" class="nav-link pt-2 pb-2 btn-paynow cl-paynow font-type-primary font-size-septenary align-self-center text-center" id="btn-responsive-mobile">Pay Now</a>
         </li>
@@ -33,7 +31,7 @@
 
 <header id="responsive-header-mobile">
   <nav class="d-flex flex-row justify-content-between container-header p-2">
-    <img class="img-fluid ml-5 logo-size mt-1 mb-1" id="logo" src="{{ asset('assets/mobile-logo.svg') }}" height="50px" alt="logo-brand" />
-    <img src="{{ asset('assets/icon_menu_mobile.svg') }}" alt="Menu" class="img-fluid ham-mobile mr-5 mt-1 mb-1" id="toggler-menu">
+    <img class="img-fluid ml-3 logo-size mt-1 mb-1" id="logo" src="{{ asset('assets/mobile-logo.svg') }}" height="50px" alt="logo-brand" />
+    <img src="{{ asset('assets/icon_menu_mobile.svg') }}" alt="Menu" class="img-fluid ham-mobile mr-3 mt-1 mb-1" id="toggler-menu">
   </nav>
 </header>
